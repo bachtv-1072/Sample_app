826ad65b21eef978ac534b58922971040b309b61
bachtv-1072 <truong.viet.bach@sun-asterisk.com>
2020-12-02 14:54:42 +0700

    Chapter 5:Filling in the layout

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
-const { environment } = require("@rails/webpacker")
-
-module.exports = environment
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    jquery: "jquery",
+    "window.Tether": "tether",
+    Popper: ["popper.js", "default"] // for Bootstrap 4
+  })
+);
+module.exports = environment;
